a08e6b26bb5714231fa88281acb081ee21adb36b
Felipe Gabriel da Silva <felipegabriel1106@outlook.com>
2023-09-27 10:53:04 -0300

    Formulario de cadastro - Validação

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import './App.css'
 import NavBar from './components/navbar/Navbar'
-import Cadastro from './pages/Cadastro'
+import Cadastro from './pages/cadastro/Cadastro'
 import ListaCadastro from './pages/ListaCadastro'
 import Home from './pages/Home'
 import Footer from './components/footer/Footer'
---
+++ src/pages/cadastro/Cadastro.tsx
@@ -0,0 +1,63 @@
+import {Flex, FormControl, FormLabel, Input, RadioGroup, Radio, Button} from "@chakra-ui/react"
+import { yupResolver } from '@hookform/resolvers/yup';
+import {useForm} from "react-hook-form"
+import * as yup from "yup"
+import styles from "./Cadastro.module.css"
+
+const schema =  yup.object().shape({
+     nomeEmpresa: yup.string().required("Campo obrigatório"),
+     cnpj: yup.string().required("Campo obrigatório"),
+     email: yup.string().email().required("Campo obrigatório"),
+     descricao: yup.string().required("Campo obrigatório"),
+     endereco: yup.string().required("Campo obrigatório"),
+     parceria: yup.boolean().required("Campo obrigatório"),
+});
+
+export default function Cadastro() {
+
+     const { register, handleSubmit, formState: {errors} } = useForm({
+          mode: 'onSubmit',
+          resolver: yupResolver(schema)
+     });
+
+     const onSubmit = (data: object) => console.log(data);
+
+     return(
+          <Flex margin="2em auto" display="flex" flexDir="column" p="1.5em"  w="50%" border="1px solid #ccc" borderRadius="1em" boxShadow="0 0 0 5px #ccc" >
+               <form onSubmit={handleSubmit(onSubmit)} className={styles.formulario}>
+                    <FormControl>
+                         <FormLabel>Nome da empresa:</FormLabel>
+                         <Input type="text" placeholder="Digite o nome da empresa" defaultValue="" {...register("nomeEmpresa", {required:true})} />
+                    </FormControl>
+                    
+                    <FormControl>
+                         <FormLabel>CNPJ da empresa:</FormLabel>
+                         <Input type="text" placeholder="Digite o cnpj da empresa" defaultValue="" {...register("cnpj", {required:true})} />
+                    </FormControl>
+                    
+                    <FormControl>
+                         <FormLabel>E-mail da empresa:</FormLabel>
+                         <Input type="email" placeholder="Digite o e-mail da empresa" defaultValue="" {...register("email", {required:true})} />
+                    </FormControl>
+                    
+                    <FormControl>
+                         <FormLabel>Descrição:</FormLabel>
+                         <Input type="text" placeholder="Categoria"  defaultValue="" {...register("descricao", {required:true})} />
+                    </FormControl>
+
+                    <FormControl>
+                         <FormLabel>Endereço:</FormLabel>
+                         <Input type="text" placeholder="Informe o endereço da empresa" defaultValue=""  {...register("endereco", {required:true})} />
+                    </FormControl>
+
+                    <RadioGroup display="flex" gap="1em">
+                         <FormLabel>Parceira:</FormLabel>
+                         <Radio {...register("parceria", {required:true})} value="true" >Sim</Radio>
+                         <Radio {...register("parceria", {required:true})} value="false" >Não</Radio>
+                    </RadioGroup>
+
+                    <Button display="flex" m="auto" justifyContent="center" type="submit" w="20%">Enviar</Button>
+               </form>
+          </Flex>
+     )
+}
