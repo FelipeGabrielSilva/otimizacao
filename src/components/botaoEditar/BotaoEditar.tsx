527ae591825ed09ac8c8f7ce2e933b8b3598594f
Felipe Gabriel da Silva <felipegabriel1106@outlook.com>
2023-09-28 13:54:57 -0300

    Adicionando modal e formulario de edição

--- src/components/botaoEditar/BotaoEditar.tsx
+++ src/components/botaoEditar/BotaoEditar.tsx
@@ -1,15 +1,39 @@
 import {
+     Modal,
+     ModalOverlay,
+     ModalContent,
+     ModalHeader,
+     ModalFooter,
+     ModalBody,
+     ModalCloseButton,
      Button,
+     useDisclosure,
   } from "@chakra-ui/react";
 import { FaEdit} from "react-icons/fa"
+import Editar from "../editar/Editar";
 
 export default function BotaoEditar() {
+     const {isOpen, onOpen, onClose} = useDisclosure();
 
      return(
           <>
-          <Button size="sm" bg="#219C90" color="#fff">
+          <Button size="sm" bg="#219C90" color="#fff" onClick={onOpen}>
                     <FaEdit size={18} />
           </Button>
+
+          <Modal isOpen={isOpen} onClose={onClose} size="sm" >
+               <ModalOverlay />
+                    <ModalContent>
+                         <ModalHeader>EDITAR</ModalHeader>
+                         <ModalCloseButton />
+                         <ModalBody>
+                              <Editar/>
+                         </ModalBody>
+                         <ModalFooter>
+                              <Button onClick={onClose}>Salvar</Button>
+                         </ModalFooter>
+                    </ModalContent>
+          </Modal>
           </>
      )
 }
