e2709bd00dd06f898fa52fbe547ae82b91547b48
Felipe Gabriel da Silva <felipegabriel1106@outlook.com>
2023-09-28 12:05:48 -0300

    Alerta para exclusão do item

--- src/components/botaoDeletar/BotaoDeletar.tsx
+++ src/components/botaoDeletar/BotaoDeletar.tsx
@@ -1,10 +1,33 @@
-import { Button } from "@chakra-ui/react";
+import { AlertDialog, AlertDialogBody, AlertDialogContent, AlertDialogFooter, AlertDialogHeader, AlertDialogOverlay, Button, useDisclosure } from "@chakra-ui/react";
 import { FaTrashAlt } from "react-icons/fa"
+import React from "react"
 
 export default function BotaoEditar() {
+     const { isOpen, onOpen, onClose } = useDisclosure();
+     const cancelRef = React.useRef(null);
+
      return(
-          <Button bg="red" display="flex" m="auto" justifyContent="center" type="submit" color="#fff" w="48px">
-               <FaTrashAlt size={24} />
-               </Button>
+          <>
+          <Button size="sm" bg="red" color="#fff">
+               <FaTrashAlt size={18} onClick={onOpen} />
+          </Button>
+
+          <AlertDialog isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={onClose}>
+               <AlertDialogOverlay>
+                    <AlertDialogContent>
+                         <AlertDialogHeader textAlign="center">Deletar item da lista?</AlertDialogHeader>
+                         <AlertDialogBody textAlign="center">Após apagar, não será possível reverter essa ação.</AlertDialogBody>
+                         <AlertDialogFooter display="flex" justifyContent="space-between">
+                              <Button ref={cancelRef} onClick={onClose}>
+                                   Cancelar
+                              </Button>
+                              <Button bg="red" color="#fff" onClick={onClose}>
+                                   Deletar
+                              </Button>
+                         </AlertDialogFooter>
+                    </AlertDialogContent>
+               </AlertDialogOverlay>
+          </AlertDialog>
+          </>
      )
 }
