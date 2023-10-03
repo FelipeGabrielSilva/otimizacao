fbf8c90967a6559962f006c3ac983e8e6224d0c4
Felipe Gabriel da Silva <felipegabriel1106@outlook.com>
2023-10-03 10:28:51 -0300

    Adicionando env e conexão com banco de dados

---
+++ src/services/localService.tsx
@@ -0,0 +1,27 @@
+import {api} from "./apiService";
+import Cadastro from "../pages/cadastro/Cadastro";
+
+export const createCadastro = async (cadastro: Cadastro) => {
+     const response = await api.post("/Cadastro", cadastro);
+     return response.data;
+};
+
+export const getAllCadastro = async () => {
+     const response = await api.get("/Cadastro");
+     return response.data;
+};
+
+export const getCadastro = async (cadastroId: string) => {
+     const response = await api.get(`/Cadastro/${cadastroId}`);
+     return response.data;
+};
+
+export const updateCadastro = async (cadastroId: string, updatedData: any) => {
+     const response = await api.patch(`/local/${cadastroId}`, updatedData);
+     return response.data;
+};
+
+export const deleteCadastro = async (cadastroId: string) => {
+     const response = await api.delete(`/cadastro${cadastroId}`);
+     return response.data;
+}
